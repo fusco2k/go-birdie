7c8586931f6f2d5516d169e15446301ebe502d5f
Filipe Fusco <filipe.fusco@gmail.com>
2019-04-29 18:34:19 -0300

    basic cfg read from file

--- main.go
+++ main.go
@@ -3,13 +3,34 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
-	//creates a new scanner for reading user input from CLI
-	scanner := bufio.NewScanner(os.Stdin)
+	var cfg string
+	//opens the cfg.txt file, later implementations will use to retain the auth tokens
+	//if does no exist, create the file using the rw permission
+	file, err := os.OpenFile("cfg.txt", os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer file.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	//creates a new scanner to read the cfg file
+	s := bufio.NewScanner(file)
+	//loops scanning each new line and concatenates to the cfg string
+	for s.Scan() {
+		str := s.Text()
+		cfg += str + " "
+	}
+	//prints the file output
+	fmt.Println(cfg)
 
+	//creates a new scanner to read user input from CLI
+	scanner := bufio.NewScanner(os.Stdin)
 	//loops scanning each new line and prints to the console
 	for scanner.Scan() {
 		_, err := fmt.Println(scanner.Text())
@@ -17,4 +38,6 @@ func main() {
 			fmt.Fprintln(os.Stderr, "err: ", err)
 		}
 	}
+
+	//http.ListenAndServe(":8080", nil)
 }
