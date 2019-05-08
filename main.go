a5a6cb6f4ff160afd5f0710755cedf1c69540b53
Filipe Fusco <filipe.fusco@gmail.com>
2019-05-08 15:32:45 -0300

    refactored code, before testing, so maybe broken

--- main.go
+++ main.go
@@ -15,16 +15,40 @@ import (
 )
 
 func main() {
-
+	//gets home env key
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Panicln(err)
+	}
 	//------handling tag------
 	var cfg string
 	//create a flag to interact with the user and get the tweet message
 	tweetTag := flag.String("t", "", `tweets the message under ""`)
 	//parse the flags to use the flags content
 	flag.Parse()
-	//show instructions if no arg is used
+	//aks if wish to authenticate
 	if flag.NArg() == 0 {
-		log.Fatalln(`use -a for start a new authentication process or -t to tweet`)
+		fmt.Println("do you wish to start a new authentication process? (y/n)")
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Println("enter the twitter api key")
+		scanner.Scan()
+		authDecision := scanner.Text()
+		switch authDecision {
+		case "y":
+			//check if app folder exist
+			checkDirectory(homedir)
+			//create a new cfg.key
+			createCfg(homedir)
+			//prints the result
+			fmt.Println(`now you are authenticated, next time use the -t followed by the "...tweet text...", exiting`)
+			os.Exit(0)
+		case "n":
+			fmt.Println(`ok, next time use the -t followed by the "...tweet text...", exiting`)
+			os.Exit(0)
+		default:
+			fmt.Println("sorry, thats not an option, exiting now.")
+			os.Exit(0)
+		}
 	}
 	//crash the execution if using more than 1 argument
 	if flag.NArg() > 0 {
@@ -45,38 +69,17 @@ func main() {
 
 	//model for receive cfg keys
 	keySet := models.Key{}
-	//gets home env key
-	homedir, err := os.UserHomeDir()
-	if err != nil {
-		log.Panicln(err)
-	}
 	//check if app folder exist
-	if _, err := os.Stat(homedir + "/.go-birdie"); os.IsNotExist(err) {
-		fmt.Println("dir does not exist, creating it...")
-		//creates folders to store data
-		err := os.MkdirAll(homedir+"/.go-birdie", 0777)
-		if err != nil {
-			log.Println(err)
-		}
-
-		file, err := os.OpenFile(homedir+"/.go-birdie/cfg.key", os.O_CREATE|os.O_RDWR, 0777)
-		if err != nil {
-			log.Println(err)
-		}
-		file.WriteString(`{"authenticate":"false"}`)
-		file.Sync()
-		file.Close()
+	if !checkDirectory(homedir) {
+		log.Fatalln("you are not authenticated, start the program using no flags")
 	}
-	//opens the cfg.txt file, later implementations will use to retain the auth tokens
+	//opens the cfg.txt file
 	//if does no exist, create the file using the rw permission
 	file, err := os.OpenFile(homedir+"/.go-birdie/cfg.key", os.O_RDWR, 0666)
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Fatalf("could not open the cfg.key: %v", err)
 	}
 	defer file.Close()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
 	//creates a new scanner to read the cfg file
 	s := bufio.NewScanner(file)
 	//loops scanning each new line and concatenates to the cfg string
@@ -91,31 +94,8 @@ func main() {
 	}
 	fmt.Println(keySet)
 	//------handling keys end------
-	if keySet.Authenticated == false {
-		file.Close()
-		os.Remove(homedir + "/.go-birdie/cfg.key")
-		file, err := os.OpenFile(homedir+"/.go-birdie/cfg.key", os.O_CREATE|os.O_RDWR, 0777)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		defer file.Close()
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		//authenticate the user
-		keySet.APIKey, keySet.APISecretKey, keySet.AccessToken, keySet.AccessTokenSecret = auth.Authenticate()
-		keySet.Authenticated = true
-		//write to the cfg.key
-		file.WriteString(
-			`{"authenticated":true,
-			"api_key":"` + keySet.APIKey + `",
-			"api_secret_key":"` + keySet.APISecretKey + `",
-			"access_token":"` + keySet.AccessToken + `",
-			"access_token_secret":"` + keySet.AccessTokenSecret + `"}`)
-		file.Sync()
-	}
-	fmt.Println(keySet)
-	//------handling auth------
+
+	//------handling auth ------
 
 	//config new costumer usign the api keys
 	costumer := oauth.NewConsumer(keySet.APIKey, keySet.APISecretKey, oauth.ServiceProvider{
@@ -144,4 +124,52 @@ func main() {
 	fmt.Println(res.StatusCode)
 
 	//------handling tweet end------
+}
+
+func checkDirectory(homedir string) bool {
+	if _, err := os.Stat(homedir + "/.go-birdie"); os.IsNotExist(err) {
+		fmt.Println()
+		//creates folders to store data
+		err := os.MkdirAll(homedir+"/.go-birdie", 0777)
+		if err != nil {
+			log.Println(err)
+		}
+		return false
+	}
+	return true
+}
+
+func createCfg(homedir string) {
+	//model for receive cfg keys
+	keySet := models.Key{}
+	//remove the older cfg.key, if exists, and create a new one
+	os.Remove(homedir + "/.go-birdie/cfg.key")
+	file, err := os.OpenFile(homedir+"/.go-birdie/cfg.key", os.O_CREATE|os.O_RDWR, 0777)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer file.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	//authenticate the user
+	keySet.APIKey, keySet.APISecretKey, keySet.AccessToken, keySet.AccessTokenSecret = auth.Authenticate()
+	keySet.Authenticated = true
+	//write to the cfg.key
+	file.WriteString(
+		`{"authenticated":true,
+			"api_key":"` + keySet.APIKey + `",
+			"api_secret_key":"` + keySet.APISecretKey + `",
+			"access_token":"` + keySet.AccessToken + `",
+			"access_token_secret":"` + keySet.AccessTokenSecret + `"}`)
+	file.Sync()
+}
+
+func dummyKey() (ck, cs, at, as string){
+ck = "testeck"
+cs = "testecs"
+at = "testeat"
+as = "testeas"
+
+return ck, cs, at, as
 }
